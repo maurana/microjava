ac06447a605ea89a540116ba5673f72355edda57
Maurana <45948023+maurana@users.noreply.github.com>
2023-08-31 05:44:30 +0700

    starter

---
+++ account/src/main/java/com/microjava/account/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.microjava.account.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+	public String index() {
+		return "Account services v0.0.1";
+	}
+}
